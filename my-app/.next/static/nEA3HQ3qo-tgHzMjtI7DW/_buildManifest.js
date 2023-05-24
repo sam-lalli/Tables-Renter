b90c8eb5f976597e580552137656d448e551efbb
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2023-05-24 02:04:20 -0600

    husky removed

---
+++ my-app/.next/static/nEA3HQ3qo-tgHzMjtI7DW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/854-f7ae86644dc1b75e.js","static/chunks/pages/index-ebb0127518cc82cf.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
